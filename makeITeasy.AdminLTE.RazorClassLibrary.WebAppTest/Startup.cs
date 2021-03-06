db44bd6b53df2c874d4123d20023f34d758bfab9
Manu D <edesigau@hotmail.fr>
2021-03-06 09:02:45 +0100

    beta version

--- makeITeasy.AdminLTE.RazorClassLibrary.WebAppTest/Startup.cs
+++ makeITeasy.AdminLTE.RazorClassLibrary.WebAppTest/Startup.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using SmartBreadcrumbs.Extensions;
 
 namespace makeITeasy.AdminLTE.RazorClassLibrary.WebAppTest
 {
